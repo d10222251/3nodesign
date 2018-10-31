d7aa2e096cae6da2b069c5b902ea016691623586
d10222251 <44633363+d10222251@users.noreply.github.com>
2018-10-31 17:31:50 +0800

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,27 @@
+/* ----------------------------------
+
+Name: custom.js
+Author: Fourtabs
+Website: https://www.fourtabs.com
+Exclusively available here: https://themeforest.net/user/fourtabsthemes
+
+------------------------------------- */
+
+
+
+jQuery(document).ready(function($) {
+	"use strict";
+
+
+
+
+	lightbox.option({
+      'resizeDuration': 200,
+      'wrapAround': true
+    });
+	
+	
+	
+
+	
+});
